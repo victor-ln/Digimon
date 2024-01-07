7070ed2154d66e06d465b6fb7ed7f6e4164eeb3e
Victor Nunes <vlima-nu@student.42sp.org.br>
2024-01-07 13:31:54 -0300

    feat: Adding colors for each digimon info

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -22,9 +22,40 @@ function renderDigimonStatus(digimon) {
   const atackDigimonElement = document.getElementById("ATK-internal-div-id");
   const defenseDigimonElement = document.getElementById("DEF-internal-div-id");
 
-  hpDigimonElement.style.width = digimon.HP + "%";
-  atackDigimonElement.style.width = digimon.ATK + "%";
-  defenseDigimonElement.style.width = digimon.DEF + "%";
+  hpDigimonElement.style.width = `${digimon.HP}%`;
+  atackDigimonElement.style.width = `${digimon.ATK}%`;
+  defenseDigimonElement.style.width = `${digimon.DEF}%`;
+
+  hpDigimonElement.style.backgroundColor = getColorBasedOnHP(digimon.HP);
+  atackDigimonElement.style.backgroundColor = getColorBasedOnATK(digimon.ATK);
+  defenseDigimonElement.style.backgroundColor = getColorBasedOnDEF(digimon.DEF);
+}
+
+function calculateColorIntensity(value) {
+  /** The lower the value, the closer to
+   * white the intensity of the final color will be */
+  return 240 - value * 2.5;
+}
+
+/* Red color fixed to 255 */
+function getColorBasedOnATK(value) {
+  let colorIntensity = calculateColorIntensity(value);
+
+  return `rgb(255, ${colorIntensity}, ${colorIntensity})`;
+}
+
+/* Green color fixed to 255 */
+function getColorBasedOnHP(value) {
+  let colorIntensity = calculateColorIntensity(value);
+
+  return `rgb(${colorIntensity}, 255, ${colorIntensity})`;
+}
+
+/* Blue color fixed to 255 */
+function getColorBasedOnDEF(value) {
+  let colorIntensity = calculateColorIntensity(value);
+
+  return `rgb(${colorIntensity}, ${colorIntensity}, 255)`;
 }
 
 function chooseRandomDigimon(digimonsList) {
